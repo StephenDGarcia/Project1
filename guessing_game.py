92271dde37a22d9a0a05410fd278bd8182d0d8c0
StephenDGarcia <stephendgarcia@gmail.com>
2022-10-23 13:11:39 -0500

    10/23 update

--- guessing_game.py
+++ guessing_game.py
@@ -2,36 +2,36 @@
 Python Web Development Techdegree
 Project 1 - Number Guessing Game
 --------------------------------
-
-For this first project we will be using Workspaces. 
-
-NOTE: If you strongly prefer to work locally on your own computer, you can totally do that by clicking: File -> Download Workspace in the file menu after you fork the snapshot of this workspace.
-
 """
-
 import random
 
 
 def start_game():
-    """Psuedo-code Hints
-    
-    When the program starts, we want to:
-    ------------------------------------
-    1. Display an intro/welcome message to the player.
-    2. Store a random number as the answer/solution.
-    3. Continuously prompt the player for a guess.
-      a. If the guess greater than the solution, display to the player "It's lower".
-      b. If the guess is less than the solution, display to the player "It's higher".
-    
-    4. Once the guess is correct, stop looping, inform the user they "Got it"
-         and show how many attempts it took them to get the correct number.
-    5. Let the player know the game is ending, or something that indicates the game is over.
-    
-    ( You can add more features/enhancements if you'd like to. )
-    """
-    # write your code inside this function.
+    answer = random.randint(1, 10)
+    count = 1
+    print("Welcome player, let's play a guessing game")
+    guess = input("Pick a number between 1 and 10:  ")
+    try:
+        guess = int(guess)
+    except ValueError:
+        print("Please enter a number")
+    else:
+        while guess != answer:
+            count += 1
+            if guess > answer:
+                print("It's lower, try again")
+                guess = int(input("Enter a new number: "))
+            else:
+                print("It's higher, try again")
+                guess = int(input("Enter a new number: "))
+        print("Got it! It took you {} tries \n GAME OVER".format(count))
 
 
-
-# Kick off the program by calling the start_game function.
 start_game()
+"""
+again = input("Would you like to play again Y/N? ")
+if again == "Y":
+    start_game()
+else: 
+    sys.exit(0)
+"""
